1de7a8f3c5df4ebc1c3ad6c581903df11fe0237c
Nmobki <nmo@bki.dk>
2020-06-08 08:58:09 +0200

    Update CP end temp.py

--- CP end temp.py
+++ CP end temp.py
@@ -2,6 +2,8 @@
 
 from sqlalchemy import create_engine
 import pandas as pd
+import pyodbc
+import datetime
 
 
 #Read login details for Probat
@@ -9,3 +11,32 @@ credentials = pd.read_excel(r'\\filsrv01\bki\11. Økonomi\04 - Controlling\NMO\2
 user = credentials['User']['Probat read']
 password = credentials['Password']['Probat read']
 
+
+# Define server connection and SQL query:
+server = '192.168.125.161'
+db = 'BKI_IMP_EXP'
+con = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + db + ';UID=' + user + ';PWD=' + password)
+query = """ SELECT
+            	DATEADD(d,DATEDIFF(d,0,[RECORDING_DATE]),0) AS [Date]
+            	,[ROASTER]
+                ,[CUSTOMER_CODE] AS [Recipe]
+                ,AVG([FINAL_TEMP_ROASTING] / 10.0) AS [End temp]
+            FROM [dbo].[PRO_EXP_BATCH_DATA_ROASTER]
+            GROUP BY
+            	DATEADD(d,DATEDIFF(d,0,[RECORDING_DATE]),0)
+            	,[ROASTER]
+                ,[CUSTOMER_CODE] """
+
+# Read query and create Profit calculation:
+df = pd.read_sql(query, con)
+
+print(df)
+
+
+# Create timestamp and other variables
+now = datetime.datetime.now()
+scriptName = 'CP end temp.py'
+executionId = int(now.timestamp())
+tType = 'Change point detection, end temperature'
+roasters = df.ROASTER.unique()
+recipes = df.Recipe.unique()
